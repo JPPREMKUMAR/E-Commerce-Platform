b3c04d90bcaee1ea8831335e88969e723e532989
JPPREMKUMAR <jppremkumar012@gmail.com>
2025-10-15 14:22:09 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ dotenv.config()
 const connectDb = async () => {
 
     try {
-        await mongoose.connect(process.env. , {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
